2f589b6b801c76933f4160e683d625069d53bc42
stephane <stephane.betton@gmail.com>
2025-05-02 21:37:11 +0200

    refactor(layout): supprimer le RouterLink des imports inutilisés

--- libs/share/ui/src/lib/layout/layout.component.ts
+++ libs/share/ui/src/lib/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzNoAnimationModule } from 'ng-zorro-antd/core/no-animation';
@@ -20,7 +20,6 @@ import { NgOptimizedImage } from '@angular/common';
     NzMenuModule,
     NzLayoutModule,
     RouterOutlet,
-    RouterLink,
     NzButtonModule,
     NzToolTipModule,
     SiderMenuItemComponent,
